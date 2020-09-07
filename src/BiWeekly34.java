c5e82df974a68b11d63c7e486b75d676e101885d
Hemant Dhanuka <dhanuka.hemant30@gmail.com>
2020-09-07 08:15:25 +0530

    Biweekly 34 and weekly 205 Solutions

---
+++ src/BiWeekly34.java
@@ -0,0 +1,220 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class BiWeekly34 {
+    public static void main(String[] args) {
+        BiWeekly34 biWeekly34 = new BiWeekly34();
+        int ans =biWeekly34.findLengthOfShortestSubarray(new int[]{1});
+    }
+
+    public int findLengthOfShortestSubarray(int[] arr) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(i!=0){
+                if(arr[i]<arr[i-1]){
+                    break;
+                }
+            }
+            list1.add(arr[i]);
+        }
+
+        if(list1.size()==arr.length){
+            return 0;
+        }
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i=arr.length-1;i>=0;i--){
+            if(i!=arr.length-1){
+                if(arr[i]>arr[i+1]){
+                    break;
+                }
+            }
+            list2.add(arr[i]);
+        }
+
+        Collections.reverse(list2);
+        int max = Math.max(list1.size(),list2.size());
+
+        int i=0;
+        int j=0;
+
+        while(i<list1.size() && j<list2.size()){
+            if(list1.get(i)<=list2.get(j)){
+                max = Math.max(max,i+1+(list2.size()-j));
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return arr.length-max;
+    }
+
+
+    int mod = 1000000007;
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int n = locations.length;
+        int dp[][] = new int[n][fuel+1];
+        for(int d[]:dp){
+            Arrays.fill(d,-1);
+        }
+        City[] cities = new City[n];
+        for(int i =0;i<n;i++){
+          cities[i] = new City(i,locations[i]);
+        }
+
+        Arrays.sort(cities);
+        HashMap<Integer,City> map = new HashMap<>();
+        int startCityPosition = -1;
+        for(int i=0;i<cities.length;i++){
+            if(cities[i].cityNo==start){
+                startCityPosition=i;
+            }
+        }
+
+        int ans = calc(startCityPosition,fuel,finish,cities,dp);
+        System.out.println(ans);
+        return ans;
+    }
+
+    private int calc(int startCityPosition, int fuel, int finish, City[] cities, int[][] dp) {
+        if(dp[startCityPosition][fuel]!=-1){
+            return dp[startCityPosition][fuel];
+        }
+        int temp = startCityPosition+1;
+        long sum = 0;
+        if(cities[startCityPosition].cityNo==finish){
+            sum++;
+        }
+        while(temp<cities.length && cities[temp].location-cities[startCityPosition].location<=fuel){
+            sum += calc(temp,fuel-(cities[temp].location-cities[startCityPosition].location), finish,cities,dp);
+            sum %=mod;
+            temp++;
+        }
+
+        temp = startCityPosition-1;
+        while(temp>=0 && cities[startCityPosition].location-cities[temp].location<=fuel){
+            sum += calc(temp,fuel-(cities[startCityPosition].location-cities[temp].location), finish,cities,dp);
+            sum %=mod;
+            temp--;
+        }
+        int ans = (int)sum;
+        dp[startCityPosition][fuel] = ans;
+        return ans;
+
+    }
+
+    class City implements Comparable<City>{
+        int cityNo;
+        int location;
+
+        public City(int cityNo, int location) {
+            this.cityNo = cityNo;
+            this.location = location;
+        }
+
+        @Override
+        public int compareTo(City city) {
+            return this.location - city.location;
+        }
+
+        @Override
+        public String toString() {
+            return "City{" +
+                    "cityNo=" + cityNo +
+                    ", location=" + location +
+                    '}';
+        }
+    }
+
+
+
+    public int diagonalSum(int[][] mat) {
+
+        int size = mat.length;
+        int sum =0;
+        for(int i=0;i<size;i++){
+            sum += mat[i][i];
+        }
+
+        int row= 0;
+        int col = size-1;
+        for(int i=0;i<size;i++){
+            if(row+i != col-i){
+            sum += mat[row+i][col-i];
+            }
+        }
+        return sum;
+    }
+
+    public int numWays(String s) {
+        int oneCount= 0;
+        int n = s.length();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                oneCount++;
+            }
+        }
+
+        if(s.length()<3 || oneCount%3!=0){
+            return 0;
+        }
+        if(oneCount==0){
+            return (int)((((n-1)*(long)(n-2))/2)%mod);
+        }
+
+        int eachStringOne = oneCount/3;
+
+        int i=0;
+        int count =0;
+        while(true){
+            if(s.charAt(i)=='1'){
+                count++;
+            }
+            if(count==eachStringOne){
+                break;
+            }
+            i++;
+        }
+
+        int j =i+1;
+        while(true){
+            if(s.charAt(j)=='1'){
+                break;
+            }
+            j++;
+        }
+
+        int diff1 = j-i;
+
+        i = n-1;
+        count=0;
+        while (true){
+            if(s.charAt(i)=='1'){
+                count++;
+            }
+            if(count==eachStringOne){
+                break;
+            }
+            i--;
+        }
+
+        j = i-1;
+        while(true){
+            if(s.charAt(j) == '1'){
+                break;
+            }
+            j--;
+        }
+
+        int diff2 = i-j;
+
+        return (int)((diff1*(long)diff2)%mod);
+    }
+
+
+
+
+}
---
+++ src/Weekly205.java
@@ -0,0 +1,177 @@
+import java.util.HashMap;
+
+public class Weekly205 {
+    public static void main(String[] args) {
+        Weekly205 weekly205 = new Weekly205();
+        weekly205.maxNumEdgesToRemove(4, new int[][]{
+                {3,1,2},
+                {3,2,3},
+                {1,1,3},
+                {1,2,4},
+                {1,1,2},
+                {2,3,4}
+        });
+    }
+
+
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        int removed1 = fun(3,1,edges,n);
+        int removed2 = fun(3,2,edges,n);
+        if(removed1==-1 || removed2 == -1){
+            return -1;
+        }
+        else return removed1+removed2;
+    }
+
+    private int fun(int type3, int otherType, int[][] edges, int n) {
+        int parent[] = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+        int totalComponents = n;
+
+        //union find for type3 edge
+        int removed =0;
+        for(int i=0;i<edges.length;i++){
+            int[] edge = edges[i];
+            if(edge[0]==type3){
+                int u = edge[1]-1;
+                int v = edge[2]-1;
+
+                int parentU = find(u, parent);
+                int parentV = find(v, parent);
+
+                if(parentU==parentV){
+                    removed++;
+                }else{
+                    parent[parentU] = parentV;
+                    totalComponents--;
+                }
+            }
+        }
+        if(otherType==2){
+            removed = 0;
+        }
+
+        for(int i=0;i<edges.length;i++){
+            int[] edge = edges[i];
+            if(edge[0]==otherType){
+                int u = edge[1]-1;
+                int v = edge[2]-1;
+
+                int parentU = find(u,parent);
+                int parentV = find(v,parent);
+
+                if(parentU==parentV){
+                    removed++;
+                }else{
+                    parent[parentU] = parentV;
+                    totalComponents--;
+                }
+            }
+        }
+
+        if(totalComponents==1){
+            return removed;
+        }else{
+            return -1;
+        }
+    }
+
+    private int find(int u, int[] parent) {
+        if(parent[u]==u){
+            return u;
+        }
+
+        int p = find(parent[u],parent);
+        parent[u] = p;
+        return p;
+    }
+
+
+    public int minCost(String s, int[] cost) {
+        int max=cost[0];
+        int sum = cost[0];
+        for(int i=1;i<s.length();i++){
+            sum = sum+cost[i];
+            if(s.charAt(i)==s.charAt(i-1)){
+                max = Math.max(max,cost[i]);
+            }else{
+                sum -=max;
+                max = cost[i];
+            }
+        }
+
+        sum -=max;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public int numTriplets(int[] nums1, int[] nums2) {
+        int triplet = getAns(nums1, nums2)+getAns(nums2,nums1);
+        System.out.println(triplet);
+        return triplet;
+    }
+
+    private int getAns(int[] nums1, int[] nums2) {
+        HashMap<Long,Integer> map1 = new HashMap<>();
+
+        for(int i = 0; i< nums1.length; i++){
+            for(int j = i+1; j< nums1.length; j++){
+                long prod = nums1[i]*(long) nums1[j];
+
+                if(!map1.containsKey(prod)){
+                    map1.put(prod,0);
+                }
+                map1.put(prod,map1.get(prod)+1);
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i< nums2.length; i++){
+            long val = nums2[i]*(long) nums2[i];
+            int freq = 0;
+            if(map1.containsKey(val)){
+                freq = map1.get(val);
+            }
+            ans +=freq;
+        }
+        return ans;
+    }
+
+    public String modifyString(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+
+            char ch = s.charAt(i);
+
+            if(ch=='?'){
+                String temp = "abc";
+                char insert = 'a';
+                for(int j=0;j<temp.length();j++){
+                    char op = temp.charAt(j);
+                    if(i>0){
+                        if(ans.charAt(i-1)==op){
+                            continue;
+                        }
+                    }
+                    if(i<s.length()-1){
+                        if(op==s.charAt(i+1)){
+                            continue;
+                        }
+                    }
+                    insert = op;
+                    break;
+                }
+
+                ans.append(insert);
+            }else{
+                ans.append(ch);
+            }
+
+
+        }
+        System.out.println(ans.toString());
+        return ans.toString();
+
+    }
+}
